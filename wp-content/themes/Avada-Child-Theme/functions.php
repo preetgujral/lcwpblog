2a23fd5dd599b3d93efbc1d64d732455e2aa3c99
PREET GUJRAL <PREET@PREETGUJRAL.COM>
2016-06-05 18:55:53 -0700

    attempted to add function to display custom data types in categories and tag slug, did not work, commented out

--- wp-content/themes/Avada-Child-Theme/functions.php
+++ wp-content/themes/Avada-Child-Theme/functions.php
@@ -11,10 +11,30 @@ function avada_lang_setup() {
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
 
-add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+// function my_cptui_add_post_types_to_archives( $query ) {
+// 	// We do not want unintended consequences.
+// 	if ( is_admin() || ! $query->is_main_query() ) {
+// 		return;
+// 	}
+//
+// 	if ( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+// 		$cptui_post_types = cptui_get_post_type_slugs();
+//
+// 		$query->set(
+// 			'post_type'
+// 			array_merge(
+// 				array( 'post' ),
+// 				$cptui_post_types
+// 			)
+// 		);
+// 	}
+// }
+// add_filter( 'pre_get_posts', 'my_cptui_add_post_types_to_archives' );
 
 function add_my_post_types_to_query( $query ) {
 	if ( is_home() && $query->is_main_query() )
 		$query->set( 'post_type', array( 'post', 'opeds' ) );
 	return $query;
 }
+
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
